525d13bdd4f9e5c38ba523dc8c4d53755f7dfa8b
Ahmedsaed <ahmedsaed2652003@gmail.com>
2022-03-10 12:00:16 +0200

    Day 4 using cpp

--- 2015/Day4/Day4.cpp
+++ 2015/Day4/Day4.cpp
@@ -1,3 +1,28 @@
-/*
-    Fk C++, there is no easy way to encrypt md5 hash using c++, while you can do the same in just 3 lines of python code
-*/
+#include <iostream>
+#include <string>
+#include "Poco/MD5Engine.h"
+#include "Poco/DigestStream.h"
+
+using namespace std;
+
+int main() {
+    Poco::MD5Engine md5;
+    string hash = "iwrupvqb", result = "";
+    int hashnum = 0;
+
+    while (true)
+    {
+        md5.update(hash + to_string(hashnum));
+        result = Poco::DigestEngine::digestToHex(md5.digest());
+        
+        if ("000000" == result.substr(0, 6)) {
+            cout << hashnum << endl;
+            break;
+        }
+        else {
+            hashnum++;
+            // if (hashnum % 10000 == 0)
+            //     cout << hashnum << endl;
+        }
+    }
+}
