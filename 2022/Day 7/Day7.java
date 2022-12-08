fd156318514d644c2975ea20a354cdaded05a9f1
Ahmedsaed <ahmedsaed2652003@gmail.com>
2022-12-09 01:04:18 +0200

    Day 7

--- 2022/Day 7/Day7.java
+++ 2022/Day 7/Day7.java
@@ -14,28 +14,32 @@ public class Day7 {
 
         while ((st = bf.readLine()) != null) {
             String[] words = st.split(" ");
-            if (words[1] == "cd") {
-                if (words[2] == "..") path.remove(path.size()-1);
+
+            if (words[1].equals("cd")) {
+                if (words[2].equals("..")) path.remove(path.size()-1);
                 else path.add(words[2]);
             } 
-            else if (words[1] == "ls" || words[0] == "dir") continue;
+            else if (words[1].equals("ls") || words[0].equals("dir")) continue;
             else {
                 for (int i = 1; i < path.size()+1; i++) {
-                    dir.put(String.join("/", path.subList(0, i)), Integer.parseInt(words[0]));
+                    String s = String.join("/", path.subList(0, i));
+                    if (dir.get(s) == null) dir.put(s, 0);
+                    dir.put(s, dir.get(s)+Integer.parseInt(words[0]));
                 }
             }
         }
-
-        int p1 = 0, p2 = (int)1e9;
+        int p1 = 0;
+        long p2 = (long)1e9;
+        long r = 30000000 - (70000000 - dir.get("/"));
 
         for (Enumeration<String> i = dir.keys(); i.hasMoreElements();) {
             String s = i.nextElement();
             int size = dir.get(s);
-            System.out.printf("%s -> %d", s, size);
+
             if (size <= 100000) {
                 p1 += size;
             }
-            if (size >= 1e9) {
+            if (size >= r) {
                 p2 = (p2 < size) ? p2 : size;  
             }
         }
