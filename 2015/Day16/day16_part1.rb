207157c23fb1a86aa9b7f2bd14cb5bbe83ce5625
Ahmedsaed <ahmedsaed2652003@gmail.com>
2024-10-11 23:35:09 +0300

    2015 day16

---
+++ 2015/Day16/day16_part1.rb
@@ -0,0 +1,36 @@
+File.open("./input.txt", 'r') do |f|
+	target_aunt = {
+		"children" => 3,
+		"cats" => 7,
+		"samoyeds" => 2,
+		"pomeranians" => 3,
+		"akitas" => 0,
+		"vizslas" => 0,
+		"goldfish" => 5,
+		"trees" => 3,
+		"cars" => 2,
+		"perfumes" => 1
+	}
+
+	best_match = ["", 0]
+
+	f.each_line do |line|
+		current_score = 0
+		aunt, props = line.split(":", 2)
+		
+		props.split(",") do |p|
+			key = p.split(':')[0].strip
+			value = p.split(':')[1].to_i
+			if target_aunt[key] == value
+				current_score += 1
+			end
+		end
+		
+		if current_score > best_match[1]
+			best_match[0] = aunt
+			best_match[1] = current_score
+		end			
+	end
+
+	puts "#{best_match[0]} has score of #{best_match[1]}"
+end
